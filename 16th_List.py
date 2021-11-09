68c52ac26f2146494c557a03ef246af69301b5c6
Cristian Arias <scaag92@gmail.com>
2021-11-09 12:14:25 -0500

    test

--- 16th_List.py
+++ 16th_List.py
@@ -1,6 +1,20 @@
 ##################################################################
 ##################################################################
 
+# List-specific operations and methods
+list[i] = x # Replaces the element at index i with x
+list.append(x) #Inserts x at the end of the list
+list.insert(i, x) #Inserts x at index i
+list.pop(i) # Returns the element a index i, also removing it from the list. If i is omitted, the last element is returned and removed.
+list.remove(x) # Removes the first occurrence of x in the list
+list.sort() # Sorts the items in the list
+list.reverse() # Reverses the order of items of the list
+list.clear() # Removes all the items of the list
+list.copy() # Creates a copy of the list
+list.extend(other_list) #Appends all the elements of other_list at the end of list
+
+##################################################################
+##################################################################
 def get_word(sentence, n):
 	# Only proceed if n is positive 
 	if n > 0:
--- test1.py
+++
@@ -1,4 +0,0 @@
-groups_per_user = {"local": ["admin", "userA"]}
-r = "test"
-groups_per_user["local"].append(r)
-print(groups_per_user)

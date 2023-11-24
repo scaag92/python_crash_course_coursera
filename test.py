596054ec08243a84357c8e5bd80a35c1a888a1a6
scaag92 <scaag92@gmail.com>
2023-11-23 21:21:44 -0500

    16th_List.py

--- test.py
+++ test.py
@@ -1,10 +1,5 @@
-num1 = 0
-num2 = 0
 
-for x in range(5):
-    print (x)
-    num1 = x
-    for y in range(14):
-        num2 = y + 3
 
-print(num1 + num2)
+word = "hello" 
+
+print(word[1:-1]+word[-1]+word[0]+"ay")
